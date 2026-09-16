2b7300faf42abca8125f819f2fdb91b2b9f2a68b
RoxAngel <sch@Rox>
2026-09-17 01:25:28 +0530

    Interpretation of a few Syntax.

---
+++ arm64.js
@@ -0,0 +1,20 @@
+// ARM64 OPCDES
+/*
+ * NAME CODE
+ * MOV  0001
+ * SVC  0011
+ * ADD  0010
+ * SUB  0110
+ * CMP  0100
+ * ORR  0101
+ * AND  0111
+ * LDR  0110
+ * STR  0000
+ * LDRB 1000
+ * STRB 1001
+ */
+export class ARM64_OPCODES{
+    constructor(
+        this.
+    )
+}
