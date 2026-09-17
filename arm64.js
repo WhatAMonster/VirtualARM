dc8f0f26e8bba5a5d4909ec59d67b2c5377748f1
WhatAMonster <ishant.26866@sscbs.du.ac.in>
2026-09-17 22:54:04 +0530

    Connecting the pieces

--- arm64.js
+++ arm64.js
@@ -13,8 +13,19 @@
  * LDRB 1000
  * STRB 1001
  */
-export class ARM64_OPCODES{
-    constructor(
-        this.
-    )
-}
+export const ARM64_OPCODES = {
+    MOV:  0x01000000,
+    ADD:  0x02000000,
+    SVC:  0x03000000,
+    SUB:  0x06000000,
+    CMP:  0x04000000,
+    ORR:  0x05000000,
+    AND:  0x07000000,
+    LDR:  0x08000000
+};
+
+export const HARDWARE_MASKS = {
+    REG_MASKS: 0x1F,         // isolates bits 4-0
+    OPCODE_MASK: 0xFF000000  // isolates top 8 bits for decoding
+};
+
