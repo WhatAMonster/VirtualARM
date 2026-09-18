1c4b46a9b2cf47b37e05edaea9d5ad499a98ff47
RoxAngel <sch@Rox>
2026-09-19 00:03:17 +0530

    minor fixes

--- process.js
+++ process.js
@@ -4,7 +4,7 @@ export class ARMProcessor{
     constructor(){
         this.registers=new BigInt64Array(32);//31 registers and one zero register
         this.PC=0n;//Program Counter to track address
-        this.flags={n:0,z:0,c:0,v:0}; //CPSR Flags
+
     }
     /*
      *Executing a single 32-bit ARM Instruction
@@ -18,7 +18,7 @@ export class ARMProcessor{
         //----------------------DECODER--------------------
         //SYNTAX - OPCODE RD(Destination), RN(Operand), RM(Operand) bitwise and to get the real code
         const rd=ins & HARDWARE_MASKS.REG_MASKS //Bits 4-0 Destination
-        const rn=(ins>>5) & HARDWARE_MASKS.REG_MASKS // Bits 9-5 Source 1
+        const rn=(ins>>5) & HARDWARE_MASKS.REG_MASKS // Bits 9-5 Source
         //we gonna keep space to adhere to real arm enginnering for operations like lsr, lsl etc without changing the overall map and keeping the run unidirectionals
         const rm=(ins>>16) & HARDWARE_MASKS.REG_MASKS //Bits 20-16 Source 2
         const opc=(ins & HARDWARE_MASKS.OPCODE_MASK) >>> 0; //used >>> 0 to treat output as unsigned 32 bit because its opcode
