61d78e839c480004aae7c57f2e0bddf5ab72a2cc
WhatAMonster <ishant.26866@sscbs.du.ac.in>
2026-09-19 22:54:04 +0530

    Initializing setup for a 4MB RAM!

--- arm64.js
+++ arm64.js
@@ -16,4 +16,29 @@ export const HARDWARE_MASKS = {
     REG_MASKS: 0x1F,         // isolates bits 4-0
     OPCODE_MASK: 0xFF000000  // isolates top 8 bits for decoding
 };
+//memory 4MBs of RAM :- 2^2+2^20=2^22Bytes === 4194304 Bytes
+export class memo{
+    constructor(){
+        this.RAM=new Uint8Array(4194304); //unsigned
+        this.view=new DataView(this.RAM.buffer); //scale for measuring n bits at a time
+    }
+    /*
+     memtype fetch:-
+     000-Write Page
+     001-Write Word
+     010-Write Half-word
+     011-Write Byte
+     100-Read Page
+     101-Read Word
+     110-Read Half-Word
+     111-Read Byte
+
+     */
+    readWord(addr){this.view.getUint32(addr, true);}// true stands for little endianess --> first byte(left most) is least significant
+    writeWord(addr, val){this.view.setUint32(addr, val, true);}
+    readHWord(addr){this.view.getUint16(addr, true);}
+    writeHWord(addr){this.view.setUint16(addr, val, true);}
+    readByte(addr){this.view.getUint8(addr, true);}
+    writeByte(addr){this.view.setUint8(addr, value, true);}
+}
 
